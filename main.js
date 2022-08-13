7316c3738125f158d41990955d937c6616b54812
TNT-B <thanhbinh8223@gmail.com>
2022-08-14 01:01:54 +0700

    update login complete

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
 function signup(e) {
     event.preventDefault();
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var user = {
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let con_password = document.getElementById("con_password").value;
+    let user = {
       username: username,
       email: email,
       password: password,
     };
-    var json = JSON.stringify(user);
-    localStorage.setItem(username, json);
-    alert("dang ky thanh cong");
+    if(username == '' || email == '' || password == '' || con_password == '')
+    {
+      alert("User information cannot be left blank, please try again !")
+    }
+    else{
+      let json = JSON.stringify(user);
+      localStorage.setItem(username, json);
+      alert("Sign up successfully !");
+    }
+    
   }
+
+
   function login(e) {
     event.preventDefault();
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var user = localStorage.getItem(username);
-    var data = JSON.parse(user);
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let user = localStorage.getItem(username);
+    let data = JSON.parse(user);
     if (!user) {
-      alert("vui long nhap username password");
+      alert("Please enter username password");
     } else if (
       username == data.username &&
       email == data.email &&
       password == data.password
     ) {
-      alert("dang nhap thanh cong");
-      window.location.href = "todolist.html";
+      alert("Login successfully !");
+      window.location.href = "index.html";
     } else {
-      alert("dang nhap that bai");
+      alert("Login failed, please try again !");
     }
   }
